aa18445c2a46770c082eeeaef7be1b5294e042b7
unknown <usuario@PUERTA18.CORP.AR>
2022-06-22 13:51:53 -0300

    actualizacion

--- Cliente/dist/index.js
+++ Cliente/dist/index.js
@@ -1,16 +1,43 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // 1. Select the div element using the id property
 const app = document.getElementById("app");
-// 2. Create a new <p></p> element programmatically
-const p = document.createElement("p");
-// 3. Add the text content
-p.textContent = "a";
-// 4. Append the p element to the div element
-app === null || app === void 0 ? void 0 : app.appendChild(p);
+//Defino un array de tipo Socie, lo inicio vacio 
+let socies = [];
+//Fetch que devuelve los datos
 fetch("http://localhost:5000/socies/")
     .then(resp => {
     resp.json()
         .then(data => {
-        console.log(data.mensaje);
+        socies = data.socies;
     });
+});
+//For loop de cada socie dentro del array
+socies.forEach(socie => {
+    let p_nombre = document.createElement("p");
+    p_nombre.textContent = socie.nombre;
+    app === null || app === void 0 ? void 0 : app.appendChild(p_nombre);
+});
+socies.forEach(socie => {
+    let tr = document.createElement("tr");
+    //Nombre
+    let td_nombre = document.createElement("td");
+    td_nombre.textContent = socie.nombre;
+    //Apellido
+    let td_apellido = document.createElement("td");
+    td_nombre.textContent = socie.apellido;
+    //Nick
+    let td_nick = document.createElement("td");
+    td_nombre.textContent = socie.nick;
+    //Edad
+    let td_edad = document.createElement("td");
+    td_nombre.textContent = socie.edad.toString(10);
+    tr === null || tr === void 0 ? void 0 : tr.appendChild(td_nombre);
+    tr === null || tr === void 0 ? void 0 : tr.appendChild(td_edad);
+    tr === null || tr === void 0 ? void 0 : tr.appendChild(td_nick);
+    tr === null || tr === void 0 ? void 0 : tr.appendChild(td_edad);
+    app === null || app === void 0 ? void 0 : app.appendChild(tr);
+    let table = document.getElementById("table_socies");
+    console.log(tr);
+    table === null || table === void 0 ? void 0 : table.appendChild(tr);
 });
--- Cliente/index.ts
+++ Cliente/index.ts
@@ -1,18 +1,51 @@
+import { Socie } from "./models/socie.model";
+
 // 1. Select the div element using the id property
 const app = document.getElementById("app");
-// 2. Create a new <p></p> element programmatically
-const p = document.createElement("p");
-// 3. Add the text content
-p.textContent = "a";
-// 4. Append the p element to the div element
-app?.appendChild(p);
-
-
-    fetch("http://localhost:5000/socies/")
+//Defino un array de tipo Socie, lo inicio vacio 
+let socies: Array<Socie>=[];
+//Fetch que devuelve los datos
+fetch("http://localhost:5000/socies/")
         .then(resp => {
             resp.json()
             .then (data => {
-                    console.log(data.mensaje);
+                socies = data.socies;
             })
         });
-        
+//For loop de cada socie dentro del array
+socies.forEach(socie => {
+    let p_nombre = document.createElement("p");
+    p_nombre.textContent = socie.nombre;
+    app?.appendChild(p_nombre);
+});
+
+socies.forEach(socie => {
+    let tr = document.createElement("tr");
+    //Nombre
+    let td_nombre = document.createElement("td");
+    td_nombre.textContent=socie.nombre;
+    
+    //Apellido
+    let td_apellido = document.createElement("td");
+    td_nombre.textContent=socie.apellido;
+    
+    //Nick
+    let td_nick = document.createElement("td");
+    td_nombre.textContent=socie.nick;
+
+    //Edad
+    let td_edad = document.createElement("td");
+    td_nombre.textContent=socie.edad.toString(10);
+
+    tr?.appendChild(td_nombre);
+    tr?.appendChild(td_edad);
+    tr?.appendChild(td_nick);
+    tr?.appendChild(td_edad);
+    app?.appendChild(tr);
+
+    let table = document.getElementById("table_socies");
+
+    console.log(tr);
+    
+    table?.appendChild(tr);
+});
