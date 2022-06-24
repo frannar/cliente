750746f5a45adb41e85e8700abec69d1092c97e9
unknown <usuario@PUERTA18.CORP.AR>
2022-06-24 13:49:39 -0300

    actualizacion

--- Cliente/dist/index.js
+++ Cliente/dist/index.js
@@ -2,42 +2,49 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // 1. Select the div element using the id property
 const app = document.getElementById("app");
-//Defino un array de tipo Socie, lo inicio vacio 
+const table = document.getElementById("socies-table");
 let socies = [];
-//Fetch que devuelve los datos
-fetch("http://localhost:5000/socies/")
+fetch("http://localhost:5000/socies")
     .then(resp => {
     resp.json()
         .then(data => {
-        socies = data.socies;
+        data.socies.forEach((socie) => {
+            let tr = document.createElement("tr");
+            //link
+            let link = document.createElement("a");
+            link.href = "http://localhost:5000/socie/" + socie.nick;
+            link.innerText = socie.nombre;
+            //td
+            let td_nombre = document.createElement("td");
+            td_nombre.appendChild(link);
+            let td_apellido = document.createElement("td");
+            td_apellido.textContent = socie.apellido;
+            let td_nick = document.createElement("td");
+            td_nick.textContent = socie.nick;
+            let td_edad = document.createElement("td");
+            td_edad.textContent = socie.edad.toString(10);
+            let td_button = document.createElement("button");
+            td_button.textContent = "Mostrar";
+            td_button.onclick = function () {
+                showSocie(socie);
+            };
+            tr === null || tr === void 0 ? void 0 : tr.appendChild(td_nombre);
+            tr === null || tr === void 0 ? void 0 : tr.appendChild(td_apellido);
+            tr === null || tr === void 0 ? void 0 : tr.appendChild(td_nick);
+            tr === null || tr === void 0 ? void 0 : tr.appendChild(td_edad);
+            tr === null || tr === void 0 ? void 0 : tr.appendChild(td_button);
+            table === null || table === void 0 ? void 0 : table.appendChild(tr);
+        });
     });
 });
-//For loop de cada socie dentro del array
-socies.forEach(socie => {
-    let p_nombre = document.createElement("p");
-    p_nombre.textContent = socie.nombre;
-    app === null || app === void 0 ? void 0 : app.appendChild(p_nombre);
-});
-socies.forEach(socie => {
-    let tr = document.createElement("tr");
-    //Nombre
-    let td_nombre = document.createElement("td");
-    td_nombre.textContent = socie.nombre;
-    //Apellido
-    let td_apellido = document.createElement("td");
-    td_nombre.textContent = socie.apellido;
-    //Nick
-    let td_nick = document.createElement("td");
-    td_nombre.textContent = socie.nick;
-    //Edad
-    let td_edad = document.createElement("td");
-    td_nombre.textContent = socie.edad.toString(10);
-    tr === null || tr === void 0 ? void 0 : tr.appendChild(td_nombre);
-    tr === null || tr === void 0 ? void 0 : tr.appendChild(td_edad);
-    tr === null || tr === void 0 ? void 0 : tr.appendChild(td_nick);
-    tr === null || tr === void 0 ? void 0 : tr.appendChild(td_edad);
-    app === null || app === void 0 ? void 0 : app.appendChild(tr);
-    let table = document.getElementById("table_socies");
-    console.log(tr);
-    table === null || table === void 0 ? void 0 : table.appendChild(tr);
-});
+function showSocie(socie) {
+    fetch("http://localhost:5000/socie/" + socie.nick)
+        .then(resp => {
+        resp.json()
+            .then(data => {
+            let p = document.createElement("p");
+            p.textContent = data.socie.nombre;
+            app === null || app === void 0 ? void 0 : app.appendChild(p);
+        });
+    });
+}
--- Cliente/index.ts
+++ Cliente/index.ts
@@ -1,51 +1,58 @@
 import { Socie } from "./models/socie.model";
-
 // 1. Select the div element using the id property
 const app = document.getElementById("app");
-//Defino un array de tipo Socie, lo inicio vacio 
-let socies: Array<Socie>=[];
-//Fetch que devuelve los datos
-fetch("http://localhost:5000/socies/")
-        .then(resp => {
-            resp.json()
-            .then (data => {
-                socies = data.socies;
-            })
+const table = document.getElementById("socies-table");
+let socies: Array<Socie>=[   
+];
+
+
+fetch("http://localhost:5000/socies")
+.then(resp => {
+        resp.json()
+        .then (data => {
+            data.socies.forEach((socie:Socie) => {
+                let tr = document.createElement("tr");
+                //link
+                let link = document.createElement("a");
+                link.href = "http://localhost:5000/socie/"+ socie.nick
+                link.innerText = socie.nombre;
+                //td
+                let td_nombre = document.createElement("td");
+                td_nombre.appendChild(link)
+
+                let td_apellido = document.createElement("td");
+                td_apellido.textContent = socie.apellido;
+                
+                let td_nick = document.createElement("td");
+                td_nick.textContent = socie.nick;
+
+                let td_edad = document.createElement("td");
+                td_edad.textContent = socie.edad.toString(10);
+
+                let td_button = document.createElement("button");
+                td_button.textContent = "Mostrar";
+                td_button.onclick = function() {
+                    showSocie(socie)
+                };
+
+                tr?.appendChild(td_nombre);
+                tr?.appendChild(td_apellido);
+                tr?.appendChild(td_nick);
+                tr?.appendChild(td_edad);
+                tr?.appendChild(td_button);
+                table?.appendChild(tr);
         });
-//For loop de cada socie dentro del array
-socies.forEach(socie => {
-    let p_nombre = document.createElement("p");
-    p_nombre.textContent = socie.nombre;
-    app?.appendChild(p_nombre);
-});
+    }) 
+});                 
 
-socies.forEach(socie => {
-    let tr = document.createElement("tr");
-    //Nombre
-    let td_nombre = document.createElement("td");
-    td_nombre.textContent=socie.nombre;
-    
-    //Apellido
-    let td_apellido = document.createElement("td");
-    td_nombre.textContent=socie.apellido;
-    
-    //Nick
-    let td_nick = document.createElement("td");
-    td_nombre.textContent=socie.nick;
-
-    //Edad
-    let td_edad = document.createElement("td");
-    td_nombre.textContent=socie.edad.toString(10);
-
-    tr?.appendChild(td_nombre);
-    tr?.appendChild(td_edad);
-    tr?.appendChild(td_nick);
-    tr?.appendChild(td_edad);
-    app?.appendChild(tr);
-
-    let table = document.getElementById("table_socies");
-
-    console.log(tr);
-    
-    table?.appendChild(tr);
-});
+function showSocie (socie:Socie){
+    fetch("http://localhost:5000/socie/"+socie.nick)
+    .then(resp => {
+        resp.json()
+        .then (data => {
+            let p = document.createElement("p");
+            p.textContent = data.socie.nombre
+            app?.appendChild(p);
+        })
+    });
+}
